a9de77f914732f8b2a1689578a6f5fc445864a29
nextc <1391040917@qq.com>
2023-08-26 23:34:42 +0800

    添加注释

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { ref, onMounted } from 'vue'
 import VButton from '@/components/button/Button.vue'
 
+// 为什么这么写？参考：https://www.douyin.com/user/self?modal_id=7251216368719220023&showTab=favorite_collection
 const buttonRef = ref<InstanceType<typeof VButton> | null>()
 onMounted(() => {
   console.log(buttonRef.value)
