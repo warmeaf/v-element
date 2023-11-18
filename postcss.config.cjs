9aa937240e7fc74651e49bccd11c0a48cdea5580
nextc <1391040917@qq.com>
2023-11-18 21:38:54 +0800

    使用 postcss 插件生成 主题色和辅助色

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,11 @@
 /* eslint-env node */
 module.exports = {
   plugins: [
-    require('postcss-each'),
+    require('postcss-each')({
+      plugins: {
+        beforeEach: [require('postcss-for'), require('postcss-color-mix')]
+      }
+    }),
     require('tailwindcss/nesting'),
     require('autoprefixer'),
     require('tailwindcss')
