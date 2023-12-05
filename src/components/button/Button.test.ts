fac5ea489f909cf8799d70664f1a721756d9b5b8
nextc <1391040917@qq.com>
2023-12-05 23:12:34 +0800

    体验测试 Button 组件的 icon 属性

--- src/components/button/Button.test.ts
+++ src/components/button/Button.test.ts
@@ -2,6 +2,7 @@ import { describe, test, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 import Button from './Button.vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import VIcon from '@/components/icon/Icon.vue'
 
 describe('Button.vue', () => {
   test('basic button', () => {
@@ -61,5 +62,26 @@ describe('Button.vue', () => {
     // 判断是否存在
     expect(iconHtml.exists()).toBeTruthy()
     expect(iconHtml.attributes('icon')).toBe('arrow-up')
+  })
+
+  test('icon', () => {
+    const wrapper = mount(Button, {
+      props: {
+        loading: true
+      },
+      slots: {
+        default: 'loading'
+      },
+      global: {
+        stubs: ['VIcon']
+      }
+    })
+
+    console.log(wrapper.html())
+    // 寻找对应组件
+    const iconHtml = wrapper.findComponent(VIcon)
+    expect(iconHtml.exists()).toBeTruthy()
+    expect(iconHtml.attributes('icon')).toBe('spinner')
+    expect(wrapper.classes()).toContain('is-disabled')
   })
 })
