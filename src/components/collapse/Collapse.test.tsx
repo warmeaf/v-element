5f0da2f0be7ddf3c41fe97faa3e70e375217c22c
nextc <1391040917@qq.com>
2023-12-08 15:08:36 +0800

    完善 collapse 组件的单元测试

--- src/components/collapse/Collapse.test.tsx
+++ src/components/collapse/Collapse.test.tsx
@@ -1,26 +1,58 @@
-import { describe, test } from 'vitest'
+import { describe, test, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
-import Collapse from './Collapse.vue'
-import CollapseItem from './CollapseItem.vue'
+import VCollapse from './Collapse.vue'
+import VCollapseItem from './CollapseItem.vue'
 
 describe('Collapse.vue', () => {
-  test('collapse basice', () => {
-    const wrapper = mount(Collapse, {
-      props: {
-        modelValue: ['a']
-      },
-      slots: {
-        default: () => (
-          <CollapseItem name="a" title="aaa">
-            你好
-          </CollapseItem>
-        )
-      },
-      global: {
-        stubs: ['VIcon']
+  test('collapse basice', async () => {
+    const wrapper = mount(
+      () => (
+        <VCollapse modelValue={['a']}>
+          <VCollapseItem title="标题a" name="a">
+            内容a
+          </VCollapseItem>
+          <VCollapseItem title="标题b" name="b">
+            内容b
+          </VCollapseItem>
+          <VCollapseItem title="标题c" name="c" disabled>
+            内容c
+          </VCollapseItem>
+        </VCollapse>
+      ),
+      {
+        global: {
+          stubs: ['VIcon']
+        },
+        attachTo: document.body
       }
-    })
+    )
 
     console.log(wrapper.html())
+    const headers = wrapper.findAll('.v-collapse-item__header')
+    const contents = wrapper.findAll('.v-collapse-item__wrapper')
+    // 长度
+    expect(headers.length).toBe(3)
+    expect(contents.length).toBe(3)
+
+    // 文本
+    expect(headers[0].text()).toBe('标题a')
+    expect(contents[0].text()).toBe('内容a')
+
+    // 可见性
+    expect(contents[0].isVisible()).toBeTruthy()
+    expect(contents[1].isVisible()).toBeFalsy()
+    expect(contents[2].isVisible()).toBeFalsy()
+
+    // 行为
+    await headers[0].trigger('click')
+    console.log(contents[0].html())
+    expect(contents[0].isVisible()).toBeFalsy()
+    await headers[1].trigger('click')
+    expect(contents[1].isVisible()).toBeTruthy()
+
+    // disabled
+    expect(headers[2].classes()).toContain('is-disabled')
+    await headers[2].trigger('click')
+    expect(contents[2].isVisible()).toBeFalsy()
   })
 })
