e0ef462b0d51b1529a1458c9601fe229181d2148
nextc <1391040917@qq.com>
2023-12-08 11:34:02 +0800

    体验 tsx

---
+++ src/App02.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import VNode from './VNode'
+</script>
+
+<template>
+  <v-node msg="帅锅会"></v-node>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import App from './App02.vue'
 import './main.css'
 import '@/style/index.css'
 
