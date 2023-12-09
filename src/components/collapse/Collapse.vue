0af22958e492afcfe70ac4544ee2b345d650f43e
nextc <1391040917@qq.com>
2023-12-09 13:02:36 +0800

    优化 Collapse 组件的写法

--- src/components/collapse/Collapse.vue
+++ src/components/collapse/Collapse.vue
@@ -5,7 +5,8 @@
 </template>
 
 <script lang="ts" setup>
-import { provide, ref, watch } from 'vue'
+import { provide, computed } from 'vue'
+// import { provide, ref, watch } from 'vue'
 import type { NameType, CollapseProps, CollapseEmits } from './types'
 import { collapseItemContextKey } from './types'
 
@@ -17,34 +18,39 @@ const props = defineProps<CollapseProps>()
 const emits = defineEmits<CollapseEmits>()
 
 // 写法1
-const activeNames = ref<NameType[]>(props.modelValue)
-watch(
-  () => props.modelValue,
-  (val) => {
-    activeNames.value = val
-  }
-)
+// const activeNames = ref<NameType[]>(props.modelValue)
+// watch(
+//   () => props.modelValue,
+//   (val) => {
+//     activeNames.value = val
+//   }
+// )
 
-// 写法2（这是错误写法）
-// const activeNames = computed(() => props.modelValue)
+// 写法2
+const activeNames = computed(() => props.modelValue)
 
 if (props.accordion && props.modelValue.length > 1) {
   console.warn('accordion 模式只能有一个激活状态的数据')
 }
 const handleItemClick = (item: NameType): void => {
+  const _activeNames = [...activeNames.value]
   if (props.accordion) {
-    activeNames.value[0] = activeNames.value[0] === item ? '' : item
+    _activeNames[0] = _activeNames[0] === item ? '' : item
   } else {
-    const index = activeNames.value.indexOf(item)
+    const index = _activeNames.indexOf(item)
     if (index > -1) {
-      activeNames.value.splice(index, 1)
+      _activeNames.splice(index, 1)
     } else {
-      activeNames.value.push(item)
+      _activeNames.push(item)
     }
   }
 
-  emits('update:modelValue', activeNames.value)
-  emits('change', activeNames.value)
+  // // 之前的写法，事件参数是一个响应式对象
+  // emits('update:modelValue', activeNames.value)
+  // emits('change', activeNames.value)
+  // 更改后的写法
+  emits('update:modelValue', _activeNames)
+  emits('change', _activeNames)
 }
 provide(collapseItemContextKey, {
   activeNames,
