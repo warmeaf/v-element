739199f616b4059f16be4f25dfc3f90c0797b2e0
nextc <1391040917@qq.com>
2023-12-11 21:04:32 +0800

    调整 vitepress 的主题颜色

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,6 +3,8 @@ import { ElementPlusContainer } from '@vitepress-demo-preview/component'
 import '@vitepress-demo-preview/component/dist/style.css'
 
 import '../../../src/style/index.css'
+import './custom.css'
+
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 library.add(fas)
