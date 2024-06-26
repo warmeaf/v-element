7575a259d9c3a4482222ba0da27a96f763cc0242
nextc <1391040917@qq.com>
2024-06-26 16:27:56 +0800

    feat: 初步配置库打包选项

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import { resolve } from 'path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -16,5 +17,16 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    // 库模式配置
+    lib: {
+      // 配置打包入口
+      entry: resolve(__dirname, 'src/index.ts'),
+      // 全局变量名称
+      name: 'VElement',
+      // 文件名称
+      fileName: 'v-element'
+    }
   }
 })
