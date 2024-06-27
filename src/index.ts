0c58a2cf093f806098c76d99566c6fc8d7fca332
nextc <1391040917@qq.com>
2024-06-27 14:48:17 +0800

    fix: 修复打包后无法显示图标的问题

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import type { App } from 'vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 import VButton from '@/components/button'
 import VCollapse, { VCollapseItem } from '@/components/collapse'
 import VMessage, { createMessage, closeAll as closeMessageAll } from '@/components/message'
 import VTooltip from '@/components/tooltip'
 import VDropdown from '@/components/dropdown'
 
-const components = [VButton, VCollapse, VCollapseItem, VMessage, VTooltip, VDropdown]
+library.add(fas)
 
+const components = [VButton, VCollapse, VCollapseItem, VMessage, VTooltip, VDropdown]
 const install = (app: App) => {
   components.forEach((component) => {
     app.component(component.name as string, component)
